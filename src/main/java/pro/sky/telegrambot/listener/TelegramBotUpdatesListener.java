68989dcb3f95809a87c89cd118878325e03c2e78
amba404 <amba.avg@gmail.com>
2025-04-17 20:49:13 +0900

    TgBot is just working

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -2,10 +2,15 @@ package pro.sky.telegrambot.listener;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Message;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.model.request.ReplyParameters;
+import com.pengrad.telegrambot.request.SendMessage;
+import com.pengrad.telegrambot.response.SendResponse;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -14,10 +19,13 @@ import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
-    @Autowired
-    private TelegramBot telegramBot;
+    private final TelegramBot telegramBot;
+
+    public TelegramBotUpdatesListener(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
 
     @PostConstruct
     public void init() {
@@ -26,11 +34,42 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
 
     @Override
     public int process(List<Update> updates) {
-        updates.forEach(update -> {
-            logger.info("Processing update: {}", update);
-            // Process your updates here
-        });
+        updates.forEach(this::processUpdate);
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
+    private void processUpdate(Update update) {
+        logger.info("Processing update: {}", update);
+        // Process your updates here
+        String text = update.message().text();
+        if (text.equals("/start")) {
+            sendHello(update);
+        } else {
+            sendSimpleMessage(update, "Я не понял... :-( ");
+//            throw new IllegalStateException("Unexpected value: " + update.message().text());
+        }
+    }
+
+    private void sendSimpleMessage(@NotNull Update update, String text) {
+        Long chatId = update.message().chat().id();
+        SendMessage request = new SendMessage(chatId, text)
+                .parseMode(ParseMode.HTML)
+                .disableNotification(true)
+                .replyParameters( new ReplyParameters(update.message().messageId())) ;
+
+        SendResponse sendResponse = telegramBot.execute(request);
+        boolean ok = sendResponse.isOk();
+        Message message = sendResponse.message();
+    }
+
+    private void sendHello(@NotNull Update update) {
+
+        String text = "Hello " +
+                update.message().chat().firstName() +
+                " aka " +
+                update.message().chat().username() +
+                "!";
+
+        sendSimpleMessage(update, text);
+    }
 }
