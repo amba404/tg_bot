c83ba5a3ff2b613c98673c87908e38e967e9e7ce
amba404 <amba.avg@gmail.com>
2025-04-17 18:06:38 +0900

    TgBot is just working

--- src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
+++ src/main/java/pro/sky/telegrambot/configuration/TelegramBotConfiguration.java
@@ -1,7 +1,6 @@
 package pro.sky.telegrambot.configuration;
 
 import com.pengrad.telegrambot.TelegramBot;
-import com.pengrad.telegrambot.model.DeleteMyCommands;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +14,7 @@ public class TelegramBotConfiguration {
     @Bean
     public TelegramBot telegramBot() {
         TelegramBot bot = new TelegramBot(token);
-        bot.execute(new DeleteMyCommands());
+        //bot.execute(new DeleteMyCommands());
         return bot;
     }
 
